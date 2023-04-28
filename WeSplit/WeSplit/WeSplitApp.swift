d2f4c3dc299d489bee19158730b30108fc83c7cc
andy <jhyeonc01@gmail.com>
2023-04-29 00:43:10 +0900

    Day 17

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andy C on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
