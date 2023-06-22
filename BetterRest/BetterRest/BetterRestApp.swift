fcedfae045ac9f0e662f7a01318543261f5180e6
Andy C <andy@Andys-MacBook-Air.local>
2023-06-22 22:19:54 +0900

    BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Andy C on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Andy C on 6/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+    }
+}
+
+#Preview {
+    ContentView()
+}
