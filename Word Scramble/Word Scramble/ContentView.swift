9f54fcb38892daccbd2083dd9b82dea50cd2957a
Andy C <andy@Andys-MacBook-Air.local>
2023-06-25 15:41:04 +0900

    new project

---
+++ Word Scramble/Word Scramble/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Andy C on 6/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!")
+            .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Andy C on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
