813440d5b226080ad32723e1970dfab729e957fd
Andy C <andy@Andys-MacBook-Air.local>
2023-06-28 22:40:23 +0900

    animation

---
+++ Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Andy C on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
