8df68420f3eea30c03fed7080699cc830d1e07fb
Andy C <andy@Andys-MacBook-Air.local>
2023-07-06 21:40:35 +0900

    moonshot

---
+++ Projects/Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Andy C on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
