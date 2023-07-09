b2b4a2d28a471824c4465adae02b34c3748a84d4
Andy C <andy@Andys-MacBook-Air.local>
2023-07-09 20:26:21 +0900

    LengthConverter

---
+++ Challenges/LengthConverter/LengthConverter/LengthConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConverterApp.swift
+//  LengthConverter
+//
+//  Created by Andy C on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LengthConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
