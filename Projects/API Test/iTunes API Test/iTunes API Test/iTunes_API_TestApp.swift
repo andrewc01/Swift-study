4d00e27629fd5ccf1b747d015c6573594b16d409
Andy C <jhyeonc01@gmail.com>
2023-07-28 15:57:34 +0900

    cupcake

---
+++ Projects/API Test/iTunes API Test/iTunes API Test/iTunes_API_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_API_TestApp.swift
+//  iTunes API Test
+//
+//  Created by Andy C on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_API_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Andy C on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
