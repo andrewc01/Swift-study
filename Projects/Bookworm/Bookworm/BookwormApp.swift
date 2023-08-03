7c230ac66f1bdf1e270cdfec0fb7f954c9e5f955
Andy C <jhyeonc01@gmail.com>
2023-08-03 22:01:26 +0900

    preparing for bookworm

---
+++ Projects/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Andy C on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+        }
+    }
+}
---
+++ Projects/Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Andy C on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    ContentView()
+}
