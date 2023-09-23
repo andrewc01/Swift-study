e97fa2dbe0a9443986dbe37c5eba42d011d2feda
Andy C <jhyeonc01@gmail.com>
2023-09-23 23:47:04 +0900

    started project 14 part 1

---
+++ Projects/Bucket List/Bucket List/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Bucket List
+//
+//  Created by Andy C on 9/23/23.
+//
+
+import SwiftUI
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed")
+    }
+}
+
+struct ContentView: View {
+    var loadingState = LoadingState.loading
+    
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
