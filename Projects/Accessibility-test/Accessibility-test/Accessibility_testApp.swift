90e821a6fb220887fd6106af03e2ce94764e2b29
Andy C <jhyeonc01@gmail.com>
2023-10-21 12:21:47 +0900

    implementing voiceover

---
+++ Projects/Accessibility-test/Accessibility-test/Accessibility_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessibility_testApp.swift
+//  Accessibility-test
+//
+//  Created by Andy C on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Accessibility_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
