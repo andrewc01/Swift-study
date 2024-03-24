adce91ec42dd7dda1c218f26937b8b96f9059a38
Andy <andy@TensorBook.local>
2024-03-24 20:08:34 +0900

    Project 2

---
+++ SwiftUI Fundamentals/AppleFrameworks/AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Andy on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
