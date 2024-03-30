b0f41c8d16e38c12592e03f1a1a97180b16ed4b0
Andy <andy@TensorBook.local>
2024-03-30 11:14:27 +0900

    Added Error

--- 100 Days of SwiftUI/Playground/SwiftEnum.playground/Contents.swift
+++ 100 Days of SwiftUI/Playground/SwiftEnum.playground/Contents.swift
@@ -1,22 +1,26 @@
 import SwiftUI
+import NaturalLanguage
 
-enum Weekday {
-    case monday
-    case tuesday
-    case wednesday
-    case thursday
-    case friday
+let text = "The American Red Cross was established in Washington, D.C., by Clara Barton."
+
+
+let tagger = NLTagger(tagSchemes: [.language])
+tagger.string = text
+
+
+let options: NLTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+let tags: [NLTag] = [.personalName, .placeName, .organizationName]
+
+
+tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .word, scheme: .nameType, options: options) { tag, tokenRange in
+    // Get the most likely tag, and print it if it's a named entity.
+    if let tag = tag, tags.contains(tag) {
+        print("\(text[tokenRange]): \(tag.rawValue)")
+    }
+        
+    // Get multiple possible tags with their associated confidence scores.
+    let (hypotheses, _) = tagger.tagHypotheses(at: tokenRange.lowerBound, unit: .word, scheme: .nameType, maximumCount: 1)
+    print(hypotheses)
+        
+   return true
 }
-
-var day = Weekday.monday
-day = Weekday.tuesday
-day = Weekday.friday
-
-// same as
-enum Weekday2 {
-    case monday, tuesday, wednesday, thursday, friday
-}
-
-var day2 = Weekday2.monday
-day2 = .tuesday
-day2 = .friday
