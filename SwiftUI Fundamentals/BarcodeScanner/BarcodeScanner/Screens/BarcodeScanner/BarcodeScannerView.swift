ea7ff75ab666d07a91c91715c048fe22aad4dadc
Andy <andy@TensorBook.local>
2024-03-30 12:58:30 +0900

    Project 3 completed

--- SwiftUI Fundamentals/BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ SwiftUI Fundamentals/BarcodeScanner/BarcodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -8,23 +8,34 @@
 import SwiftUI
 
 struct BarcodeScannerView: View {
+    
+    @StateObject var viewModel = BarcodeScannerViewModel()
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Rectangle()
+                ScannerView(scannedCode: $viewModel.scannedCode, 
+                            alertItem: $viewModel.alertItem)
                     .frame(maxWidth:.infinity, maxHeight: 300)
                 
                 Spacer().frame(height: 80)
                 
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
-                Text("Not Yet Scanned")
+                
+                Text(viewModel.statusText)
                     .bold()
                     .font(.largeTitle)
-                    .foregroundColor(.green)
+                    .foregroundColor(viewModel.statusTextColor)
                     .padding()
+
             }
             .navigationTitle("Barcode Scanner")
+            .alert(item: $viewModel.alertItem) { alertItem in
+                Alert(title: Text(alertItem.title),
+                      message: Text(alertItem.message),
+                      dismissButton: alertItem.dismissButton)
+            }
         }
     }
 }
