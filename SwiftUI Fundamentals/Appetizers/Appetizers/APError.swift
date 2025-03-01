caac665008e4729d79f651f22479e96504310140
jhyeonc <jhyeonc01@gmail.com>
2025-03-02 05:23:32 +0900

    Added Network Manager

---
+++ SwiftUI Fundamentals/Appetizers/Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Andy on 2025-03-02.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
