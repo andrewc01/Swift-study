7b5e7d7a3590feb2916c70f80a8aea42fe31f27d
jhyeonc <jhyeonc01@gmail.com>
2025-03-02 22:59:02 +0900

    Created loading screen

---
+++ SwiftUI Fundamentals/Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Andy on 2025-03-02.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimaryColor = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimaryUIColor = UIColor(named: "brandPrimary")
+}
